c8af15296d75c90f3c6d44720c317efe5bff8cf3
stubhub9 <stubox8@yahoo.com>
2024-07-09 22:39:25 -0700

    creating project templates; fun with borders, outline and margin

--- projects/project2024july8/script8july24.js
+++ projects/project2024july8/script8july24.js
@@ -1,6 +1,10 @@
 var $title = $('.js-title');
 var copy   = '.js-copy';
 
+/*  jQuery slideToggle
+    Toggle slideUp/Down for all 
+    <p> elements $("p").slideToggle();
+*/
 $title.click(function () {
   $(this).next(copy).slideToggle();
   $(this).parent().siblings().children().next().slideUp();
---
+++ projects/project_templates/script_template.js
@@ -0,0 +1,3 @@
+/*
+Is there a minimal Js file, for these(?) projects; for the typical "website" with banners and stuff.
+*/
