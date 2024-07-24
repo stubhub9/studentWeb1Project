52bbfbbc422e2ad7917f31328c799cd589806fe5
stubhub9 <stubox8@yahoo.com>
2024-07-23 22:50:58 -0700

    updated index some, JS studies including eventListening

--- projects/project2024july21/script24july21.js
+++ projects/project2024july21/script24july21.js
@@ -1,4 +1,5 @@
 document.body.onload = addElement;
+document.body.onload = addElement;
 
 function addElement() {
     //  create a new div element
@@ -31,6 +32,58 @@ function newpage() {
     let thename = document.getElementById("someName1").value;
     document.open();
     document.write("<h1>Welcome! "+ thename + "</h1>");
+    document.write("<h2>Click REFRESH to return.</h2>");
     document.close();
 
 }
+
+//  onBlur, onFocus, 
+let pNum = document.getElementById("phoneNum1"),
+    helpTxt = document.getElementById("reminder1");
+
+pNum.onfocus = function() {
+    helpTxt.innerHTML = "Format:  123-456-7890";
+}
+pNum.onblur = function() {
+    helpTxt.innerHTML = "";
+}
+
+
+//  Buttons doing stuff
+
+let button1 = document.getElementById("btn1");
+let textblock1 = document.getElementById("buttonsOutput");
+button1.addEventListener("click", function(event) {
+    textblock1.innerHTML = "The "+ event.type + event.currentTarget + event.detail + event.target + "for Button1 did this.";
+});
+
+
+//  Header/ Viewer for 3 items.
+let slinks = [document.getElementById("sumlink2"),
+    document.getElementById("statslink2"),
+    document.getElementById("comlink2")],
+
+    sdivs = [document.getElementById("sumdiv2"),
+        document.getElementById("statsdiv2"),
+        document.getElementById("comdiv2")];
+
+function change_div (count2) {
+    slinks[count2].addEventListener("click", function(event) {
+        event.preventDefault();
+        sdivs[count2].style.display = "block";
+        for (let j = 0; j < sdivs.length; j++ ) {
+            if (j === count2) {
+                continue;
+            }
+            else {
+                sdivs[j].style.display = "none";
+            }
+        }
+    }, false);
+}
+
+for (let i = 0; i < slinks.length; i++) {
+    change_div(i);
+}
+sdivs[1].style.display = "none";
+sdivs[2].style.display = "none";
